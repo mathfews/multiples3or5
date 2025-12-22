aa8c2a4b3c81e71cb0992352e9cbe872ed11c4f4
Matheus Felipe <matheusfcw1010@gmail.com>
2025-12-21 21:01:01 -0300

    log multiples using arithmetic progression

---
+++ website/script.js
@@ -0,0 +1,16 @@
+x = 1
+num = 10
+while ( (3*x) < num) {
+    let list = []
+    list.push( (3 +(x-1)*3 ) )
+    x += 1
+    console.log(list)
+}
+console.log("-")
+x = 1
+while ( (5*x) < num ) {
+    let list2 = []
+    list2.push( (5 + (x-1)*5) )
+    x += 1
+    console.log(list2)
+}
