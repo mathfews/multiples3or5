5828cf3aaf3c300068bd313254761b7388a5fdd5
Matheus Felipe <matheusfcw1010@gmail.com>
2025-12-21 21:34:29 -0300

    implement array intersection logic using filter and includes

--- website/script.js
+++ website/script.js
@@ -1,16 +1,18 @@
 x = 1
-num = 10
+num = 35
+const list2 = []
+const list = []
 while ( (3*x) < num) {
-    let list = []
     list.push( (3 +(x-1)*3 ) )
     x += 1
-    console.log(list)
 }
-console.log("-")
+console.log(list)
 x = 1
+console.log("---")
 while ( (5*x) < num ) {
-    let list2 = []
     list2.push( (5 + (x-1)*5) )
     x += 1
-    console.log(list2)
 }
+console.log(list2)
+
+const intersection = list.filter(num => list2.includes(num))
