5737b577d6a77ce45b6bdf2d9850c195b4367295
Matheus Felipe <matheusfcw1010@gmail.com>
2025-12-25 21:49:37 -0300

    complete solution for sum of multiples challenge

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # multiples3and5
 A CodeWars challenge that helped me identify gaps in my early JavaScript learning
+
+
--- website/script.js
+++ website/script.js
@@ -1,18 +1,27 @@
-x = 1
-num = 35
-const list2 = []
-const list = []
-while ( (3*x) < num) {
-    list.push( (3 +(x-1)*3 ) )
-    x += 1
+function multiples(num) {
+    if (num < 0) {
+        return 0
+    }
+    else {
+        x = 1
+        sum = 0
+        const multiples3 = []
+        const multiples5 = []
+        while ( (3*x) < num) {
+            multiples3.push( (3 +(x-1)*3 ) )
+            x += 1
+        }
+        x = 1
+        while ( (5*x) < num ) {
+            multiples5.push( (5 + (x-1)*5 ) )
+            x += 1
+        }
+        const multiples = multiples3.filter(num => !multiples5.includes(num)).concat(multiples5)
+        x = 0
+        while (x < multiples.length) {
+            sum += multiples[x]
+            x += 1
+        }
+        return sum
+    }
 }
-console.log(list)
-x = 1
-console.log("---")
-while ( (5*x) < num ) {
-    list2.push( (5 + (x-1)*5) )
-    x += 1
-}
-console.log(list2)
-
-const intersection = list.filter(num => list2.includes(num))
